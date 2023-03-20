f801d6d401ba3198dc4821e09ed623209b1f014f
MilohPadgett <92548375+MilohPadgett@users.noreply.github.com>
2023-03-20 14:52:13 -0700

    Update mainpage.py

--- src/mainpage.py
+++ src/mainpage.py
@@ -2,7 +2,7 @@
     @mainpage ME405
     @section Software_Design
     @subsection Task_Diagram
-    \image html Task_Diagram.drawio.png
+    \image html Task_Diagram_Updated.png
     @subsection Pitch_State_Diagram
     \image html Pitch_Diagram.drawio.png
     @subsection Yaw_State_Diagram
